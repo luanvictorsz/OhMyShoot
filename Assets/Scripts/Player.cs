c7301ba970dd55ec1d1f6174ae8383daadc19934
L. Victor <luanvitorlima2017@gmail.com>
2025-04-28 02:27:27 -0300

    Projeto Doom

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Others")]
+    [SerializeField] private Rigidbody2D rig;
+
+    [Header("Float")]
+    [SerializeField] private float speed;
+    [SerializeField] private float mouseSensibility;
+
+    [Header("Vector")]
+    [SerializeField] private Vector2 mouseMovement;
+    [SerializeField] private Vector2 keyboardCommand;
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Move();
+        Camera();
+    }
+
+    private void Move()
+    {
+        keyboardCommand = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 moveHorizontal = transform.up * -keyboardCommand.x;
+        Vector3 moveVertical = transform.right * keyboardCommand.y;
+
+        rig.linearVelocity = (moveHorizontal + moveVertical) * speed;
+    }
+
+    private void Camera()
+    {
+        mouseMovement = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 rotation = new Vector3(0, 0, -mouseMovement.x * mouseSensibility);
+        transform.Rotate(rotation);
+    }
+}
