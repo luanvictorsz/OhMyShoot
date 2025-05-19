454a4d81247d0f112a4ae5626143b0a42ba992a7
L. Victor <luanvitorlima2017@gmail.com>
2025-05-19 20:04:20 -0300

    adicionando script inimigo

---
+++ Assets/Scripts/Outros/Enemy.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform[] walkPoints;
+    [SerializeField] private int currentWalkPointIndex = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
