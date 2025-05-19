0f767fb287495c522ece43cbce6ac0ead792ccad
L. Victor <luanvitorlima2017@gmail.com>
2025-05-19 20:39:08 -0300

    implementando logica de movimento do inimigo

--- Assets/Scripts/Outros/Enemy.cs
+++ Assets/Scripts/Outros/Enemy.cs
@@ -5,14 +5,42 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Transform[] walkPoints;
     [SerializeField] private int currentWalkPointIndex = 0;
+
+    [Header("Booleans")]
+    [SerializeField] private bool enemyIsAlive;
+    [SerializeField] private bool enemyIsMoving;
+
     void Start()
     {
-        
+        enemyIsAlive = true;
+        enemyIsMoving = true;
+
+        transform.position = walkPoints[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void EnemyMove()
+    {
+        if (enemyIsAlive)
+        {
+            if(enemyIsMoving)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, walkPoints[currentWalkPointIndex].position, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, walkPoints[currentWalkPointIndex].position) < 0.1f)
+                {
+                    currentWalkPointIndex++;
+
+                    if (currentWalkPointIndex >= walkPoints.Length)
+                    {
+                        currentWalkPointIndex = 0;
+                    }
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Outros/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameManager instance;
+    [SerializeField] private bool playerIsAlive;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
